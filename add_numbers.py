31798e26c554f65f8b7e6e60d6740a4bb210fecf
Aswini <aswinigoud365302@gmail.com>
2026-05-28 21:16:13 +0530

    Add a program to calculate the sum of three numbers

---
+++ add_numbers.py
@@ -0,0 +1,12 @@
+# Program to add 3 numbers
+
+# Get input from user
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+num3 = float(input("Enter third number: "))
+
+# Calculate sum
+sum_of_numbers = num1 + num2 + num3
+
+# Display result
+print(f"\nThe sum of {num1}, {num2}, and {num3} is: {sum_of_numbers}")
